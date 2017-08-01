3b292a1c5acaedf3e2c99487c15595509246ef34
Kai-Rey Lee <kairey@gmail.com>
2017-08-01 10:53:04 -0700

    added submit button

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import "./HomePage.css";
 
 class HomePage extends Component {
+  
+  onURLSubmit(e) {
+    e.preventDefault();
+    console.log('enter pressed');
+  }
+  
 	render() {
 		return(
 			<div className="homepage">
@@ -12,14 +18,22 @@ class HomePage extends Component {
 					<p className="center">A space for photographers to display their photos in a simple yet aesthetically pleasing way.</p>
 				</div>
 				<div className="row">
-					<form className="col s12">
+					<form 
+            className="col s12"
+            onSubmit={this.onURLSubmit}>
 						<div className="row">
 							<div className="input-field col s6 offset-s3">
 								<input 
 									id="album_url"
 									type="text"
 									className="validate"
+                  ref={(input) => {this.textInput = input;}}
 								/>
+                <button
+                  type="submit"
+                  className="homepage-submit"
+                  hidden="hidden">
+                  Submit</button>
 								<label htmlFor="album_url" className="active">Flickr Album URL</label>
 							</div>
 						</div>
