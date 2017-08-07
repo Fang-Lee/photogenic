2de7605327f89e0634f4fc14949bb55802ad2ce0
Allen Fang <allenfang08@gmail.com>
2017-08-07 15:43:27 -0700

    fetching photo sizes

--- src/components/AlbumPage/Gallery.js
+++ src/components/AlbumPage/Gallery.js
@@ -10,6 +10,7 @@ class Gallery extends Component {
         this.state = {
             photoset: false,
             photos: [],
+            photoSizes: [],
             message: 'Loading...'
         }
         
@@ -20,10 +21,27 @@ class Gallery extends Component {
         Axios.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=5773753a6ea2c1f66d3b52ca41d3be26&photoset_id=${this.props.albumID}&user_id=${this.props.userID}&format=json&nojsoncallback=1`, {
             cancelToken: this.cancelToken.token
         }).then((result) => {
-           this.setState({
-                photoset: result.data,
-               photos: result.data.photoset.photo
-           })
+          this.setState({
+            photoset: result.data,
+            photos: result.data.photoset.photo
+          })
+          this.state.photos.forEach((photo) => {
+            Axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=5773753a6ea2c1f66d3b52ca41d3be26&photo_id=${photo.id}&format=json&nojsoncallback=1`, {
+              cancelToken: this.cancelToken.token
+            }).then((result) => {
+              this.setState({
+                photoSizes: [...this.state.photoSizes, result.data.sizes.size]
+              })
+            }).catch((err) => {
+              if(Axios.isCancel(err)) {
+                console.log('Request Canceled on getting photo sizes.');
+              } else {
+                this.setState({
+                  message: 'Photo Sizes not found'
+                })
+              }
+            })
+          })
         }).catch((err) => {
            if(Axios.isCancel(err)){
                console.log('Request Canceled');
@@ -40,8 +58,6 @@ class Gallery extends Component {
     }
     render() {
         if(this.state.photoset){
-            console.log(this.state.photoset);
-            console.log(this.state.photos);
             var photolist = this.state.photos.map((photo, index) => {
                 return <PhotoItem key={index} farm={photo.farm} id={photo.id} secret={photo.secret} title={photo.title} server={photo.server}/> 
             })
