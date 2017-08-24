d78de3a734f7e8959adbc13f6e4ef2b12079534e
Allen Fang <allenfang08@gmail.com>
2017-08-24 16:10:30 -0700

    react-ga added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import AlbumPage from './components/AlbumPage/AlbumPage';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-105227083-1');
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 class App extends Component {
 	render() {
@@ -9,8 +16,8 @@ class App extends Component {
 			<BrowserRouter>
 				<div>
 					<Switch>
-						<Route path="/:userID/:albumID" component={AlbumPage} />
-						<Route path="/" component={HomePage} />
+						<Route path="/:userID/:albumID" component={AlbumPage} onUpdate={logPageView} />
+						<Route path="/" component={HomePage} onUpdate={logPageView}/>
 					</Switch>
 				</div>
 			</BrowserRouter>
