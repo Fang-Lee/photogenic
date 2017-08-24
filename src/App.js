e2c8829121ea88adadc974df7362772322ae5948
Allen Fang <allenfang08@gmail.com>
2017-08-24 16:29:42 -0700

    new build folder

--- src/App.js
+++ src/App.js
@@ -3,24 +3,23 @@ import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import AlbumPage from './components/AlbumPage/AlbumPage';
 import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
 
 ReactGA.initialize('UA-105227083-1');
-function logPageView() {
-	console.log('google analytics');
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
+const history = createHistory()
+history.listen((location, action) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<div>
+			<BrowserRouter history={history}>
 					<Switch>
-						<Route path="/:userID/:albumID" component={AlbumPage} onEnter={logPageView} />
-						<Route path="/" component={HomePage} onEnter={logPageView}/>
+						<Route path="/:userID/:albumID" component={AlbumPage} />
+						<Route path="/" component={HomePage} />
 					</Switch>
-				</div>
 			</BrowserRouter>
 		);
 	}
